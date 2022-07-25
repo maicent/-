28aa236a418a4023d41e383e30074a57d2444bca
麦创软件设计工作室 <676888309@qq.com>
2022-07-25 10:12:51 +0800

    优化图表

--- src/views/admin/components/LineChart.vue
+++ src/views/admin/components/LineChart.vue
@@ -62,6 +62,8 @@ export default {
       this.setOptions(this.chartData)
     },
     setOptions({ reg_date, reg_num, gacha_num } = {}) {
+      const maxY1 = parseInt((Math.max(...reg_num) + 10) / 10) * 10
+      const maxY2 = parseInt((Math.max(...gacha_num) + 10) / 10) * 10
       this.chart.setOption({
         xAxis: {
           data: reg_date,
@@ -84,24 +86,47 @@ export default {
           },
           padding: [5, 10]
         },
-        yAxis: {
-          axisTick: {
-            show: false
+        yAxis: [
+          {
+            type: 'value',
+            name: '用户数',
+            min: 0,
+            max: maxY1, // 左侧y轴最大值
+            // 两个y轴的刻度必须整除一个相同的数才能重合
+            interval: Math.ceil(maxY1 / 10), // 间距等分为10等分
+            axisLabel: {
+              show: true,
+              color: '#FF005A',
+              showMinLabel: true,
+              showMaxLabel: true
+            }
+          },
+          {
+            type: 'value',
+            name: '寻访数',
+            min: 0,
+            max: maxY2, // 左侧y轴最大值
+            // 两个y轴的刻度必须整除一个相同的数才能重合
+            interval: Math.ceil(maxY2 / 10), // 间距等分为10等分
+            axisLabel: {
+              show: true,
+              color: '#3888fa',
+              showMinLabel: true,
+              showMaxLabel: true
+            }
           }
-        },
+        ],
         legend: {
           data: ['注册用户数', '寻访数']
         },
         series: [{
           name: '注册用户数', itemStyle: {
-            normal: {
-              color: '#FF005A',
-              lineStyle: {
-                color: '#FF005A',
-                width: 2
-              }
+            color: '#FF005A',
+            areaStyle: {
+              color: '#f3a8ff'
             }
           },
+          yAxisIndex: 0,
           smooth: true,
           type: 'line',
           data: reg_num,
@@ -110,17 +135,12 @@ export default {
         },
         {
           name: '寻访数', itemStyle: {
-            normal: {
-              color: '#3888fa',
-              lineStyle: {
-                color: '#3888fa',
-                width: 2
-              },
-              areaStyle: {
-                color: '#f3f8ff'
-              }
+            color: '#3888fa',
+            areaStyle: {
+              color: '#f3f8ff'
             }
           },
+          yAxisIndex: 1,
           smooth: true,
           type: 'line',
           data: gacha_num,
