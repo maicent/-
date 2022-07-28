e9877c74dcf87c14904548f2babb0264f1fd7b35
麦创软件设计工作室 <676888309@qq.com>
2022-07-28 19:33:07 +0800

    取消使用托管

--- vue.config.js
+++ vue.config.js
@@ -24,9 +24,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/ark-db'
-    : '/',
+  publicPath: '/',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
